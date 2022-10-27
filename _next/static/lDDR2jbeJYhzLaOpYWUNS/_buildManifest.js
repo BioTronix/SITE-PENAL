8ca40ca31c1c51fb7f4401ad1ef49e701e14c2b0
BioTronix <biotronicofc@gmail.com>
2022-10-27 12:50:05 -0300

    first commit

---
+++ _next/static/lDDR2jbeJYhzLaOpYWUNS/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/80-ebd33f9103440890.js","static/chunks/pages/index-62a460f5f5aba0f4.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
